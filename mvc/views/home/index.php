bf4c6a0375d6a60d56876d8588eb77f8a865c3fa
RajaWalidain <walidain378@gmail.com>
2024-10-12 21:29:38 +0700

    upload tugas 5

--- mvc/views/home/index.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>mvc/assets/css/style_home_index.css">
-<h1>Selamat Datang di Aplikasi Penjualan Barang</h1>
-<p>Ini adalah halaman home.</p>
-<a href="<?= BASEURL; ?>index.php?url=login/index">Login</a>
